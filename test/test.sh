98c3703f49e5465963c3e923c66eba6737c4bb51
zhaohuabing <zhaohuabing@gmail.com>
2021-08-05 21:01:29 +0800

    update test script
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,8 @@
 BASEDIR=$(dirname "$0")
 docker kill consumer provider
 docker rm consumer provider
-docker run -d --network host --name consumer aeraki/dubbo-sample-consumer
+docker run -d --network host --name consumer --env mode=demo aeraki/dubbo-sample-consumer
 docker run -d -p 20881:20880 --name provider aeraki/dubbo-sample-provider
 kill `ps -ef | awk '/bazel-bin\/envoy/{print $2}'`
 $BASEDIR/../bazel-bin/envoy -c $BASEDIR/test.yaml &
-while true
-do
-  sleep 3
-  curl 127.0.0.1:9009/hello
-  echo " "
-done
+docker logs -f consumer
