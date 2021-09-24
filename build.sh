2c22b2e99d60b686d337345942eb7b5871e2d63c
zhaohuabing <zhaohuabing@gmail.com>
2021-09-24 11:54:40 +0800

    Update build.sh
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- build.sh
+++ build.sh
@@ -2,26 +2,20 @@
 chmod +x bazel/get_workspace_status
 export CC=clang-10
 export CXX=clang++-10
-
 export USE_BAZEL_VERSION=$(cat .bazelversion)
 
 bazel version
-
 
 # bazel clean --expunge
 
 buildFlags=" -s --sandbox_debug --verbose_explanations --explain=build.log --verbose_failures "
 # gdb opt fastbuild
 if [ ! -z $1 ];then
-    buildFlags+=" -c $1" //dgb, opt, fastbuild
+    buildFlags+=" -c $1" #dgb, opt, fastbuild
 else
     buildFlags=$buildFlags" -c fastbuild "
 fi
 
 target="envoy"
 
-if [ `uname` = "Darwin" ];then
-    bazel build ${buildFlags} //:$target --host_force_python=PY3
-else
-   bazel build ${buildFlags} //:$target
-fi
+bazel build ${buildFlags} //:$target --host_force_python=PY3
