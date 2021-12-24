2ac5a241b1d4308f5673290b060428d9859217d5
覃士林 <wonderqin@tencent.com>
2021-12-24 13:50:34 +0800

    add local ratelimit ability

---
+++ src/meta_protocol_proxy/filters/local_ratelimit/config.cc
@@ -0,0 +1,33 @@
+#include "src/meta_protocol_proxy/filters/local_ratelimit/config.h"
+
+#include "envoy/registry/registry.h"
+#include "src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace NetworkFilters {
+namespace MetaProtocolProxy {
+namespace LocalRateLimit {
+
+FilterFactoryCb LocalRateLimitFilterConfig::createFilterFactoryFromProtoTyped(
+    const aeraki::meta_protocol_proxy::filters::local_ratelimit::v1alpha::LocalRateLimit& cfg, const std::string&,
+    Server::Configuration::FactoryContext& context) {
+
+  auto filter_config = std::make_shared<FilterConfig>(cfg, context.scope(), context.dispatcher() );
+
+  // cfg is changed
+  return [cfg, filter_config](FilterChainFactoryCallbacks& callbacks) -> void {
+    callbacks.addFilter(std::make_shared<LocalRateLimit>( filter_config ));
+  };
+}
+
+/**
+ * Static registration for the router filter. @see RegisterFactory.
+ */
+REGISTER_FACTORY(LocalRateLimitFilterConfig, NamedMetaProtocolFilterConfigFactory);
+
+} // namespace LocalRateLimit
+} // namespace MetaProtocolProxy
+} // namespace NetworkFilters
+} // namespace Extensions
+} // namespace Envoy
---
+++ src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit.cc
@@ -0,0 +1,102 @@
+#include "src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit.h"
+#include "src/meta_protocol_proxy/app_exception.h"
+#include "common/buffer/buffer_impl.h"
+#include "envoy/extensions/filters/network/local_ratelimit/v3/local_rate_limit.pb.h"
+#include "src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit_impl.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace NetworkFilters {
+namespace MetaProtocolProxy {
+namespace LocalRateLimit {
+
+FilterConfig::FilterConfig(const LocalRateLimitConfig& cfg,
+  Stats::Scope& scope, Event::Dispatcher& dispatcher) : stats_(generateStats(cfg.stat_prefix(), scope)),
+  rate_limiter_(LocalRateLimiterImpl(
+          std::chrono::milliseconds(
+              PROTOBUF_GET_MS_OR_DEFAULT(cfg.token_bucket(), fill_interval, 0)),
+          cfg.token_bucket().max_tokens(),
+          PROTOBUF_GET_WRAPPED_OR_DEFAULT(cfg.token_bucket(), tokens_per_fill, 1), dispatcher,
+          cfg.descriptors(), cfg) ),
+          config_(cfg) {}  
+
+LocalRateLimitStats FilterConfig::generateStats(const std::string& prefix, Stats::Scope& scope) {
+  const std::string final_prefix = prefix + ".local_rate_limit";
+  return {ALL_LOCAL_RATE_LIMIT_STATS(POOL_COUNTER_PREFIX(scope, final_prefix))};
+}  
+
+void LocalRateLimit::onDestroy() {
+  cleanup();
+}
+
+void LocalRateLimit::setDecoderFilterCallbacks(DecoderFilterCallbacks& callbacks) {
+  callbacks_ = &callbacks;
+}
+
+FilterStatus LocalRateLimit::onMessageDecoded(MetadataSharedPtr metadata, MutationSharedPtr) {
+
+  if (getLocalRateLimit(metadata)) {
+    // 限流， 直接返回限流状态码给到客户端
+    ENVOY_STREAM_LOG(debug, "meta protocol ratelimit:  '{}'", *callbacks_, metadata->getRequestId());
+    callbacks_->sendLocalReply(
+        AppException(Error{ErrorType::OverLimit,
+                           fmt::format("meta protocol router: no cluster match for request '{}'",
+                                       metadata->getRequestId())}),
+        false);
+    return FilterStatus::StopIteration;
+  }
+
+  ENVOY_STREAM_LOG(debug, "meta protocol local ratelimit: decoding request", *callbacks_);
+  return FilterStatus::Continue;
+}
+
+void LocalRateLimit::setEncoderFilterCallbacks(EncoderFilterCallbacks& callbacks) {
+  encoder_callbacks_ = &callbacks;
+}
+
+FilterStatus LocalRateLimit::onMessageEncoded(MetadataSharedPtr, MutationSharedPtr) {
+  return FilterStatus::Continue;
+}
+
+void LocalRateLimit::cleanup() {}
+
+bool LocalRateLimit::getLocalRateLimit(MetadataSharedPtr metadata) {
+  std::vector<RateLimit::LocalDescriptor> descriptors;
+  if (filter_config_->config_.GetDescriptor()) {
+    populateDescriptors(descriptors, metadata);
+  }
+  if (filter_config_->rate_limiter_.requestAllowed(descriptors, metadata)) {
+    filter_config_->stats_.ok_.inc();
+    return false;
+  } else {
+    filter_config_->stats_.rate_limited_.inc();
+    return true;
+  }
+
+  return false;
+};
+
+void LocalRateLimit::populateDescriptors(std::vector<RateLimit::LocalDescriptor>& descriptors,
+                           MetadataSharedPtr metadata) {                          
+  for (auto descriptorCfg : filter_config_->config_.descriptors()) {
+    RateLimit::LocalDescriptor descriptor;
+    std::vector<RateLimit::DescriptorEntry> entriesNew; 
+
+    for (auto entry : descriptorCfg.entries()) {
+      RateLimit::DescriptorEntry entryNew;
+      entryNew.key_ = entry.key();
+      entryNew.value_ = metadata->getString(entry.key());
+      entriesNew.emplace_back(entryNew);
+    }
+
+    descriptor.entries_ = entriesNew;
+    descriptors.emplace_back(descriptor);
+  }
+}
+
+
+} // namespace LocalRateLimit
+} // namespace MetaProtocolProxy
+} // namespace NetworkFilters
+} // namespace Extensions
+} // namespace Envoy
