85d01d65b7ecb490431e31dc04c41bafed5a64a6
Huabing Zhao <zhaohuabing@gmail.com>
2022-02-25 11:15:16 +0800

    optimize building process
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

---
+++ Makefile.core.mk
@@ -0,0 +1,214 @@
+## Copyright 2017 Istio Authors
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+TOP := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+
+SHELL := /bin/bash
+BAZEL_STARTUP_ARGS ?=
+BAZEL_BUILD_ARGS ?=
+BAZEL_TARGETS ?= //...
+# Don't build Debian packages and Docker images in tests.
+BAZEL_TEST_TARGETS ?= ${BAZEL_TARGETS} -tools/deb/... -tools/docker/...
+HUB ?=
+TAG ?=
+repo_dir := .
+
+ifeq "$(origin CC)" "default"
+CC := clang
+endif
+ifeq "$(origin CXX)" "default"
+CXX := clang++
+endif
+PATH := /usr/lib/llvm-10/bin:$(PATH)
+
+VERBOSE ?=
+ifeq "$(VERBOSE)" "1"
+BAZEL_STARTUP_ARGS := --client_debug $(BAZEL_STARTUP_ARGS)
+BAZEL_BUILD_ARGS := -s --sandbox_debug --verbose_failures $(BAZEL_BUILD_ARGS)
+endif
+
+ifeq "$(origin WITH_LIBCXX)" "undefined"
+WITH_LIBCXX := $(shell ($(CXX) --version | grep ^g++ >/dev/null && echo 0) || echo 1)
+endif
+ifeq "$(WITH_LIBCXX)" "1"
+BAZEL_CONFIG = --config=libc++
+else
+BAZEL_CONFIG =
+endif
+
+UNAME := $(shell uname)
+ifeq ($(UNAME),Linux)
+BAZEL_CONFIG_DEV  = $(BAZEL_CONFIG)
+BAZEL_CONFIG_REL  = $(BAZEL_CONFIG) --config=release
+BAZEL_CONFIG_ASAN = $(BAZEL_CONFIG) --config=clang-asan
+BAZEL_CONFIG_TSAN = $(BAZEL_CONFIG) --config=clang-tsan
+endif
+ifeq ($(UNAME),Darwin)
+BAZEL_CONFIG_DEV  = # macOS always links against libc++
+BAZEL_CONFIG_REL  = --config=release
+BAZEL_CONFIG_ASAN = --config=macos-asan
+BAZEL_CONFIG_TSAN = # no working config
+endif
+
+BAZEL_OUTPUT_PATH = $(shell bazel info $(BAZEL_BUILD_ARGS) output_path)
+BAZEL_ENVOY_PATH ?= $(BAZEL_OUTPUT_PATH)/k8-fastbuild/bin/src/envoy/envoy
+
+CENTOS_BUILD_ARGS ?= --cxxopt -D_GLIBCXX_USE_CXX11_ABI=1 --cxxopt -DENVOY_IGNORE_GLIBCXX_USE_CXX11_ABI_ERROR=1
+# WASM is not build on CentOS, skip it
+# TODO can we do some sort of regex?
+CENTOS_BAZEL_TEST_TARGETS ?= ${BAZEL_TARGETS} -tools/deb/... -tools/docker/... -extensions:stats.wasm -extensions:metadata_exchange.wasm -extensions:attributegen.wasm
+
+build:
+	#TODO(lambdai): Prerun v8//:build if and only if BAZEL_TARGETS depends on v8.
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_DEV) @com_googlesource_chromium_v8//:build && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_DEV) $(BAZEL_TARGETS)
+
+build_envoy:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && \
+	#bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_REL) @com_googlesource_chromium_v8//:build && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_REL) envoy
+
+build_envoy_tsan:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_TSAN) @com_googlesource_chromium_v8//:build && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_TSAN) //src/envoy:envoy
+
+build_envoy_asan:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_ASAN) @com_googlesource_chromium_v8//:build && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_ASAN) //src/envoy:envoy
+
+build_wasm:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_REL) //extensions:stats.wasm
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_REL) //extensions:metadata_exchange.wasm
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_REL) //extensions:attributegen.wasm
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_REL) @envoy//test/tools/wee8_compile:wee8_compile_tool
+	bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/stats.wasm bazel-bin/extensions/stats.compiled.wasm
+	bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/metadata_exchange.wasm bazel-bin/extensions/metadata_exchange.compiled.wasm
+	bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/attributegen.wasm bazel-bin/extensions/attributegen.compiled.wasm
+
+# NOTE: build_wasm has to happen before build_envoy, since the integration test references bazel-bin symbol link for envoy binary,
+# which will be overwritten if wasm build happens after envoy.
+check_wasm: build_wasm build_envoy
+	env GO111MODULE=on WASM=true go test -timeout 30m ./test/envoye2e/stats_plugin/...
+
+clean:
+	@bazel clean
+
+gen:
+	@scripts/gen-testdata.sh
+
+gen-check:
+	@scripts/gen-testdata.sh -c
+
+test:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_DEV) @com_googlesource_chromium_v8//:build && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_DEV) //src/envoy:envoy
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && bazel $(BAZEL_STARTUP_ARGS) test $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_DEV) -- $(BAZEL_TEST_TARGETS)
+	env ENVOY_PATH=$(BAZEL_ENVOY_PATH) GO111MODULE=on go test -timeout 30m ./...
+
+test_asan:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_ASAN) @com_googlesource_chromium_v8//:build && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_ASAN) //src/envoy:envoy
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && bazel $(BAZEL_STARTUP_ARGS) test $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_ASAN) -- $(BAZEL_TEST_TARGETS)
+	env ENVOY_PATH=$(BAZEL_ENVOY_PATH) ASAN=true GO111MODULE=on go test -timeout 30m ./...
+
+test_tsan:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_TSAN) @com_googlesource_chromium_v8//:build && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_TSAN) //src/envoy:envoy
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && bazel $(BAZEL_STARTUP_ARGS) test $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_TSAN) -- $(BAZEL_TEST_TARGETS)
+	env ENVOY_PATH=$(BAZEL_ENVOY_PATH) TSAN=true GO111MODULE=on go test -timeout 30m ./...
+
+test_centos:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(CENTOS_BUILD_ARGS) $(BAZEL_CONFIG_DEV) @com_googlesource_chromium_v8//:build && \
+	bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(CENTOS_BUILD_ARGS) $(BAZEL_CONFIG_DEV) //src/envoy:envoy
+	# TODO: re-enable IPv6 tests
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && bazel $(BAZEL_STARTUP_ARGS) test $(BAZEL_BUILD_ARGS) $(CENTOS_BUILD_ARGS) $(BAZEL_CONFIG_DEV) --test_filter="-*IPv6*" -- $(CENTOS_BAZEL_TEST_TARGETS)
+
+check:
+	@echo >&2 "Please use \"make lint\" instead."
+	@false
+
+lint: lint-copyright-banner format-go lint-go tidy-go lint-scripts
+	@scripts/check-repository.sh
+	@scripts/check-style.sh
+
+protoc = protoc -I common-protos -I extensions
+protoc_gen_docs_plugin := --docs_out=camel_case_fields=false,warnings=true,per_file=true,mode=html_fragment_with_front_matter:$(repo_dir)/
+
+attributegen_path := extensions/attributegen
+attributegen_protos := $(wildcard $(attributegen_path)/*.proto)
+attributegen_docs := $(attributegen_protos:.proto=.pb.html)
+$(attributegen_docs): $(attributegen_protos)
+	@$(protoc) -I ./extensions $(protoc_gen_docs_plugin)$(attributegen_path) $^
+
+metadata_exchange_path := extensions/metadata_exchange
+metadata_exchange_protos := $(wildcard $(metadata_exchange_path)/*.proto)
+metadata_exchange_docs := $(metadata_exchange_protos:.proto=.pb.html)
+$(metadata_exchange_docs): $(metadata_exchange_protos)
+	@$(protoc) -I ./extensions $(protoc_gen_docs_plugin)$(metadata_exchange_path) $^
+
+stats_path := extensions/stats
+stats_protos := $(wildcard $(stats_path)/*.proto)
+stats_docs := $(stats_protos:.proto=.pb.html)
+$(stats_docs): $(stats_protos)
+	@$(protoc) -I ./extensions $(protoc_gen_docs_plugin)$(stats_path) $^
+
+stackdriver_path := extensions/stackdriver/config/v1alpha1
+stackdriver_protos := $(wildcard $(stackdriver_path)/*.proto)
+stackdriver_docs := $(stackdriver_protos:.proto=.pb.html)
+$(stackdriver_docs): $(stackdriver_protos)
+	@$(protoc) -I ./extensions $(protoc_gen_docs_plugin)$(stackdriver_path) $^
+
+accesslog_policy_path := extensions/access_log_policy/config/v1alpha1
+accesslog_policy_protos := $(wildcard $(accesslog_policy_path)/*.proto)
+accesslog_policy_docs := $(accesslog_policy_protos:.proto=.pb.html)
+$(accesslog_policy_docs): $(accesslog_policy_protos)
+	@$(protoc) -I ./extensions $(protoc_gen_docs_plugin)$(accesslog_policy_path) $^
+
+extensions-docs:  $(attributegen_docs) $(metadata_exchange_docs) $(stats_docs) $(stackdriver_docs) $(accesslog_policy_docs)
+
+deb:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) $(BAZEL_CONFIG_REL) //tools/deb:istio-proxy
+
+artifacts:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) BAZEL_BUILD_ARGS="$(BAZEL_BUILD_ARGS)" && ./scripts/push-debian.sh -p "$(ARTIFACTS_GCS_PATH)" -o "$(ARTIFACTS_DIR)"
+
+test_release:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) BAZEL_BUILD_ARGS="$(BAZEL_BUILD_ARGS)" && ./scripts/release-binary.sh
+
+test_release_centos:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) BAZEL_BUILD_ARGS="$(BAZEL_BUILD_ARGS) $(CENTOS_BUILD_ARGS)" BUILD_ENVOY_BINARY_ONLY=1 BASE_BINARY_NAME=envoy-centos && ./scripts/release-binary.sh -c
+
+push_release: build
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) BAZEL_BUILD_ARGS="$(BAZEL_BUILD_ARGS)" && ./scripts/release-binary.sh -d "$(RELEASE_GCS_PATH)" -p
+
+push_release_centos:
+	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) BAZEL_BUILD_ARGS="$(BAZEL_BUILD_ARGS) $(CENTOS_BUILD_ARGS)" BUILD_ENVOY_BINARY_ONLY=1 BASE_BINARY_NAME=envoy-centos && ./scripts/release-binary.sh -c -d "$(RELEASE_GCS_PATH)"
+
+# Used by build container to export the build output from the docker volume cache
+exportcache:
+	@mkdir -p /work/out/linux_amd64
+	@cp -a /work/bazel-bin/src/envoy/envoy /work/out/linux_amd64
+	@chmod +w /work/out/linux_amd64/envoy
+	@cp -a /work/bazel-bin/**/*wasm /work/out/linux_amd64 &> /dev/null || true
+
+.PHONY: build clean test check artifacts extensions-proto
+
+include common/Makefile.common.mk
---
+++ scripts/release-binary.sh
@@ -0,0 +1,244 @@
+#!/bin/bash
+#
+# Copyright 2016 Istio Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+#
+set -ex
+
+# Use clang for the release builds.
+export PATH=/usr/lib/llvm-10/bin:$PATH
+export CC=${CC:-clang}
+export CXX=${CXX:-clang++}
+
+# Add --config=libc++ if wasn't passed already.
+if [[ "$(uname)" != "Darwin" && "${BAZEL_BUILD_ARGS}" != *"--config=libc++"* ]]; then
+  BAZEL_BUILD_ARGS="${BAZEL_BUILD_ARGS} --config=libc++"
+fi
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  BAZEL_CONFIG_ASAN="--config=macos-asan"
+else
+  BAZEL_CONFIG_ASAN="--config=clang-asan"
+fi
+
+# The bucket name to store proxy binaries.
+DST=""
+
+# Verify that we're building binaries on Ubuntu 16.04 (Xenial).
+CHECK=1
+
+# Defines the base binary name for artifacts. For example, this will be "envoy-debug".
+BASE_BINARY_NAME="${BASE_BINARY_NAME:-"envoy"}"
+
+# If enabled, we will just build the Envoy binary rather then docker images, deb, wasm, etc
+BUILD_ENVOY_BINARY_ONLY="${BUILD_ENVOY_BINARY_ONLY:-0}"
+
+# Push envoy docker image.
+PUSH_DOCKER_IMAGE=0
+
+# Support CentOS builds
+BUILD_FOR_CENTOS=0
+
+function usage() {
+  echo "$0
+    -d  The bucket name to store proxy binary (optional).
+        If not provided, both envoy binary push and docker image push are skipped.
+    -i  Skip Ubuntu Xenial check. DO NOT USE THIS FOR RELEASED BINARIES.
+        Cannot be used together with -d option.
+    -c  Build for CentOS releases. This will disable the Ubuntu Xenial check.
+    -p  Push envoy docker image.
+        Registry is hard coded to gcr.io and repository is controlled via DOCKER_REPOSITORY env var."
+  exit 1
+}
+
+while getopts d:ipc arg ; do
+  case "${arg}" in
+    d) DST="${OPTARG}";;
+    i) CHECK=0;;
+    p) PUSH_DOCKER_IMAGE=1;;
+    c) BUILD_FOR_CENTOS=1;;
+    *) usage;;
+  esac
+done
+
+echo "Destination bucket: $DST"
+
+if [ "${DST}" == "none" ]; then
+  DST=""
+fi
+
+# Make sure the release binaries are built on x86_64 Ubuntu 16.04 (Xenial)
+if [ "${CHECK}" -eq 1 ] && [ "${BUILD_FOR_CENTOS}" -eq 0 ]; then
+  if [[ "${BAZEL_BUILD_ARGS}" != *"--config=remote-"* ]]; then
+    UBUNTU_RELEASE=${UBUNTU_RELEASE:-$(lsb_release -c -s)}
+    [[ "${UBUNTU_RELEASE}" == 'xenial' ]] || { echo 'Must run on Ubuntu 16.04 (Xenial).'; exit 1; }
+  fi
+  [[ "$(uname -m)" == 'x86_64' ]] || { echo 'Must run on x86_64.'; exit 1; }
+elif [ "${CHECK}" -eq 1 ] && [ "${BUILD_FOR_CENTOS}" -eq 1 ]; then
+  # Make sure the release binaries are built on CentOS 7
+  [[ $(</etc/centos-release tr -dc '0-9.'|cut -d \. -f1) == "7" ]] || { echo "Must run on CentOS 7, got $(cat /centos-release)"; exit 1; }
+elif [ -n "${DST}" ]; then
+  echo "The -i option is not allowed together with -d option."
+  exit 1
+fi
+
+# The proxy binary name.
+SHA="$(git rev-parse --verify HEAD)"
+
+if [ -n "${DST}" ]; then
+  # If binary already exists skip.
+  # Use the name of the last artifact to make sure that everything was uploaded.
+  BINARY_NAME="${HOME}/istio-proxy-debug-${SHA}.deb"
+  gsutil stat "${DST}/${BINARY_NAME}" \
+    && { echo 'Binary already exists'; exit 0; } \
+    || echo 'Building a new binary.'
+fi
+
+# BAZEL_OUT: Symlinks don't work, use full path as a temporary workaround.
+# See: https://github.com/istio/istio/issues/15714 for details.
+# k8-opt is the output directory for x86_64 optimized builds (-c opt, so --config=release-symbol and --config=release).
+# k8-dbg is the output directory for -c dbg builds.
+for config in release release-symbol debug
+do
+  case $config in
+    "release" )
+      CONFIG_PARAMS="--config=release"
+      BINARY_BASE_NAME="${BASE_BINARY_NAME}-alpha"
+      PACKAGE_BASE_NAME="istio-proxy"
+      # shellcheck disable=SC2086
+      BAZEL_OUT="$(bazel info ${BAZEL_BUILD_ARGS} output_path)/k8-opt/bin"
+      ;;
+    "release-symbol")
+      CONFIG_PARAMS="--config=release-symbol"
+      BINARY_BASE_NAME="${BASE_BINARY_NAME}-symbol"
+      PACKAGE_BASE_NAME=""
+      # shellcheck disable=SC2086
+      BAZEL_OUT="$(bazel info ${BAZEL_BUILD_ARGS} output_path)/k8-opt/bin"
+      ;;
+    "asan")
+      # NOTE: libc++ is dynamically linked in this build.
+      PUSH_DOCKER_IMAGE=0
+      CONFIG_PARAMS="${BAZEL_CONFIG_ASAN} --config=release-symbol"
+      BINARY_BASE_NAME="${BASE_BINARY_NAME}-asan"
+      PACKAGE_BASE_NAME=""
+      # shellcheck disable=SC2086
+      BAZEL_OUT="$(bazel info ${BAZEL_BUILD_ARGS} output_path)/k8-opt/bin"
+      ;;
+    "debug")
+      CONFIG_PARAMS="--config=debug"
+      BINARY_BASE_NAME="${BASE_BINARY_NAME}-debug"
+      PACKAGE_BASE_NAME="istio-proxy-debug"
+      # shellcheck disable=SC2086
+      BAZEL_OUT="$(bazel info ${BAZEL_BUILD_ARGS} output_path)/k8-dbg/bin"
+      ;;
+  esac
+
+  export BUILD_CONFIG=${config}
+
+  echo "Building ${config} proxy"
+  BINARY_NAME="${HOME}/${BINARY_BASE_NAME}-${SHA}.tar.gz"
+  SHA256_NAME="${HOME}/${BINARY_BASE_NAME}-${SHA}.sha256"
+  # All cores are used by com_googlesource_chromium_v8:build within. 
+  # Prebuild this target to avoid stacking this ram intensive task with others.
+  # shellcheck disable=SC2086
+  bazel build ${BAZEL_BUILD_ARGS} ${CONFIG_PARAMS} @com_googlesource_chromium_v8//:build
+  # shellcheck disable=SC2086
+  bazel build ${BAZEL_BUILD_ARGS} ${CONFIG_PARAMS} //src/envoy:envoy_tar
+  BAZEL_TARGET="${BAZEL_OUT}/src/envoy/envoy_tar.tar.gz"
+  cp -f "${BAZEL_TARGET}" "${BINARY_NAME}"
+  sha256sum "${BINARY_NAME}" > "${SHA256_NAME}"
+
+  if [ -n "${DST}" ]; then
+    # Copy it to the bucket.
+    echo "Copying ${BINARY_NAME} ${SHA256_NAME} to ${DST}/"
+    gsutil cp "${BINARY_NAME}" "${SHA256_NAME}" "${DST}/"
+  fi
+
+  if [ "${BUILD_ENVOY_BINARY_ONLY}" -eq 1 ]; then
+    continue
+  fi
+
+  echo "Building ${config} docker image"
+  # shellcheck disable=SC2086
+  bazel build ${BAZEL_BUILD_ARGS} ${CONFIG_PARAMS} \
+    //tools/docker:envoy_distroless \
+    //tools/docker:envoy_ubuntu
+
+  if [ "${PUSH_DOCKER_IMAGE}" -eq 1 ]; then
+    echo "Pushing ${config} docker image"
+    # shellcheck disable=SC2086
+    bazel run ${BAZEL_BUILD_ARGS} ${CONFIG_PARAMS} \
+      //tools/docker:push_envoy_distroless \
+      //tools/docker:push_envoy_ubuntu
+  fi
+
+  if [ -n "${PACKAGE_BASE_NAME}" ]; then
+    echo "Building ${config} debian package"
+    BINARY_NAME="${HOME}/${PACKAGE_BASE_NAME}-${SHA}.deb"
+    SHA256_NAME="${HOME}/${PACKAGE_BASE_NAME}-${SHA}.sha256"
+    # shellcheck disable=SC2086
+    bazel build ${BAZEL_BUILD_ARGS} ${CONFIG_PARAMS} //tools/deb:istio-proxy
+    BAZEL_TARGET="${BAZEL_OUT}/tools/deb/istio-proxy.deb"
+    cp -f "${BAZEL_TARGET}" "${BINARY_NAME}"
+    sha256sum "${BINARY_NAME}" > "${SHA256_NAME}"
+
+    if [ -n "${DST}" ]; then
+      # Copy it to the bucket.
+      echo "Copying ${BINARY_NAME} ${SHA256_NAME} to ${DST}/"
+      gsutil cp "${BINARY_NAME}" "${SHA256_NAME}" "${DST}/"
+    fi
+  fi
+done
+
+# Exit early to skip wasm build
+if [ "${BUILD_ENVOY_BINARY_ONLY}" -eq 1 ]; then
+  exit 0
+fi
+
+# Build and publish Wasm plugins
+extensions=(stats metadata_exchange attributegen)
+TMP_WASM=$(mktemp -d -t wasm-plugins-XXXXXXXXXX)
+trap 'rm -rf ${TMP_WASM}' EXIT
+make build_wasm
+if [ -n "${DST}" ]; then
+  for extension in "${extensions[@]}"; do
+    # Rename the plugin file and generate sha256 for it
+    WASM_NAME="${extension}-${SHA}.wasm"
+    WASM_COMPILED_NAME="${extension}-${SHA}.compiled.wasm"
+    WASM_PATH="${TMP_WASM}/${WASM_NAME}"
+    WASM_COMPILED_PATH="${TMP_WASM}/${WASM_COMPILED_NAME}"
+    SHA256_PATH="${WASM_PATH}.sha256"
+    SHA256_COMPILED_PATH="${WASM_COMPILED_PATH}.sha256"
+    # shellcheck disable=SC2086
+    BAZEL_TARGET=$(bazel info ${BAZEL_BUILD_ARGS} output_path)/k8-opt/bin/extensions/${extension}.wasm
+    # shellcheck disable=SC2086
+    BAZEL_COMPILED_TARGET=$(bazel info ${BAZEL_BUILD_ARGS} output_path)/k8-opt/bin/extensions/${extension}.compiled.wasm
+    cp "${BAZEL_TARGET}" "${WASM_PATH}"
+    cp "${BAZEL_COMPILED_TARGET}" "${WASM_COMPILED_PATH}"
+    sha256sum "${WASM_PATH}" > "${SHA256_PATH}"
+    sha256sum "${WASM_COMPILED_PATH}" > "${SHA256_COMPILED_PATH}"
+    
+    # push wasm files and sha to the given bucket
+    gsutil stat "${DST}/${WASM_NAME}" \
+      && { echo "WASM file ${WASM_NAME} already exist"; continue; } \
+      || echo "Pushing the WASM file ${WASM_NAME}"
+    gsutil stat "${DST}/${WASM_COMPILED_NAME}" \
+      && { echo "WASM file ${WASM_COMPILED_NAME} already exist"; continue; } \
+      || echo "Pushing the WASM file ${WASM_COMPILED_NAME}"
+    gsutil cp "${WASM_PATH}" "${SHA256_PATH}" "${DST}"
+    gsutil cp "${WASM_COMPILED_PATH}" "${SHA256_COMPILED_PATH}" "${DST}"
+  done
+fi
