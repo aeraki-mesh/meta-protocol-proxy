1b0ca552dd461e13bd69b666cbffa4774c723db6
zhaohuabing <zhaohuabing@gmail.com>
2022-03-25 15:13:28 +0800

    add test script for hash policy
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

---
+++ test/hash_policy/test.sh
@@ -0,0 +1,8 @@
+BASEDIR=$(dirname "$0")
+docker kill consumer provider server client
+docker rm consumer provider server client
+docker run -d --network host --name client --env helloServer=localhost --env mode=demo aeraki/thrift-sample-client
+docker run -d -p 9091:9090 --name server aeraki/thrift-sample-server
+kill `ps -ef | awk '/bazel-bin\/envoy/{print $2}'`
+$BASEDIR/../../bazel-bin/envoy -c $BASEDIR/test.yaml -l debug&
+docker logs -f client
