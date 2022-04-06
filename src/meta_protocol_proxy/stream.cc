002bb934d6cb8765829c5acd776c1e5e13ce10d3
zhaohuabing <zhaohuabing@gmail.com>
2022-04-06 15:04:26 +0800

    support trpc streaming
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- src/meta_protocol_proxy/stream.cc
+++ src/meta_protocol_proxy/stream.cc
@@ -15,6 +15,7 @@ void Stream::send2upstream(Buffer::Instance& data) {
 }
 
 void Stream::send2downstream(Buffer::Instance& data, bool end_stream) {
+  ENVOY_LOG(debug, "meta protocol: send upstream response to stream {}", stream_id_);
   downstream_conn_.write(data, end_stream);
   if (end_stream) {
     // todo clean stream
