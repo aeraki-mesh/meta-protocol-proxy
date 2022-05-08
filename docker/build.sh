9e8c77b8708df25ed668e24b063061db9661b666
chenpeng <smwyzi@qq.com>
2022-05-08 17:12:25 +0800

    update docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+BASEDIR=$(dirname "$0")
+IMAGE_NAME="$1"
+
 docker buildx create --use
-docker buildx build -f ./build.Dockerfile -t smwyzi/meta-protocol-proxy-build:2022-0429-0 --platform=linux/arm64,linux/amd64 . --push
+docker buildx build -f ${BASEDIR}/build.Dockerfile -t ${IMAGE_NAME} --platform=linux/arm64,linux/amd64 . --push
