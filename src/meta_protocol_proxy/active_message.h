7a49fd521eb204b9f7f13417021ce0b45d306d89
zhaohuabing <zhaohuabing@gmail.com>
2022-06-29 19:31:49 +0800

    refactory status
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- src/meta_protocol_proxy/active_message.h
+++ src/meta_protocol_proxy/active_message.h
@@ -184,7 +184,7 @@ public:
                                    FilterIterationStartState state);
   FilterStatus applyEncoderFilters(ActiveMessageEncoderFilter* filter,
                                    FilterIterationStartState state);
-  void finalizeRequest();
+  void maybeDeferredMessage();
   void onReset();
   void onError(const std::string& what);
   MetadataSharedPtr metadata() const { return metadata_; }
