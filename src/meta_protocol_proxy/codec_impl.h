c1ce2062ba18418cdc8dfbd1cfbd7992b3ae2432
zhaohuabing <zhaohuabing@gmail.com>
2022-07-14 15:08:27 +0800

    fix set Original Message
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- src/meta_protocol_proxy/codec_impl.h
+++ src/meta_protocol_proxy/codec_impl.h
@@ -52,7 +52,7 @@ public:
   uint32_t getUint32(std::string key) const override { return properties_.getUint32(key); };
 
   void setOriginMessage(Buffer::Instance& originMessage) override {
-    origin_message_ = originMessage;
+    origin_message_.move(originMessage)
   };
   Buffer::Instance& getOriginMessage() override { return origin_message_; };
   void setMessageType(MessageType messageType) override { message_type_ = messageType; };
@@ -74,7 +74,7 @@ public:
     auto copy = std::make_shared<MetadataImpl>();
 
     Buffer::OwnedImpl originalMessage;
-    originalMessage.add(copy->origin_message_);
+    originalMessage.add(copy->getOriginMessage());
     copy->setOriginMessage(originalMessage);
 
     copy->setMessageType(getMessageType());
