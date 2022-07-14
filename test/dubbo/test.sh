7cb46f9ac37fce57d440032bf8bf0668d30b560f
zhaohuabing <zhaohuabing@gmail.com>
2022-07-14 15:58:28 +0800

    add test for traffic mirroring
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- test/dubbo/test.sh
+++ test/dubbo/test.sh
@@ -1,8 +1,9 @@
 BASEDIR=$(dirname "$0")
-docker kill consumer provider server client
-docker rm consumer provider server client
-docker run -d --network host --name consumer --env mode=demo aeraki/dubbo-sample-consumer
+docker kill consumer provider server client mirror-provider
+docker rm consumer provider server client mirror-provider
+docker run -d --network host --name consumer --env mode=test aeraki/dubbo-sample-consumer
 docker run -d -p 20881:20880 --name provider aeraki/dubbo-sample-provider
+docker run -d -p 20882:20880 --name mirror-provider aeraki/dubbo-sample-provider
 kill `ps -ef | awk '/bazel-bin\/envoy/{print $2}'`
-$BASEDIR/../../bazel-bin/envoy -c $BASEDIR/test.yaml -l debug&
+$BASEDIR/../../bazel-bin/envoy -c $BASEDIR/test.yaml -l trace&
 docker logs -f consumer
