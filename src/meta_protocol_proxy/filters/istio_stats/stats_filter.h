a6c22c1382b297ffa6ea32633cf984bb6a85c985
zhaohuabing <zhaohuabing@gmail.com>
2023-02-05 15:27:45 +0800

    fix build
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- src/meta_protocol_proxy/filters/istio_stats/stats_filter.h
+++ src/meta_protocol_proxy/filters/istio_stats/stats_filter.h
@@ -44,7 +44,7 @@ private:
 
   flatbuffers::DetachedBuffer peer_node_info_;
   IstioStats& istio_stats_;
-  std::string& local_service_;
+  const std::string& local_service_;
 };
 
 } // namespace IstioStats
