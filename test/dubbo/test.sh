382f7835b9cac95a20972286c242ef65efc7fdda
zhaohuabing <zhaohuabing@gmail.com>
2023-04-18 14:36:13 +0800

    Update test.sh

--- test/dubbo/test.sh
+++ test/dubbo/test.sh
@@ -6,5 +6,5 @@ docker run -d -p 20882:20880 --name mirror-provider ghcr.io/aeraki-mesh/dubbo-sa
 sudo docker run -d --name jaeger --env COLLECTOR_ZIPKIN_HOST_PORT=":9411" -p 14268:14268 -p 16686:16686 -p 9411:9411 jaegertracing/all-in-one:1.22
 kill `ps -ef | awk '/bazel-bin\/envoy/{print $2}'`
 $BASEDIR/../../bazel-bin/envoy -c $BASEDIR/test.yaml -l info&
-docker run --network host --name consumer --env mode=demo ghcr.io/aeraki-mesh/dubbo-sample-provider
+docker run --network host --name consumer --env mode=demo ghcr.io/aeraki-mesh/dubbo-sample-consumer
 
